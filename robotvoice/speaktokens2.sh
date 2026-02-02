1bef19789e572e18fb89d186848b56c08d6e946f
ChthonVII <cwbussard@gmail.com>
2026-02-02 10:24:07 +0900

    redo robot voice using ICAO alphabet to disambiguate letter sounds, and add robot voice sounds for undocumented tokens found so far

---
+++ robotvoice/speaktokens2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo Start
+filename='tokenssounds2.csv'
+outfilename='GuildWars.ds.robot2'
+if [[ -f "$outfilename" ]]; then
+    echo "deleting old output file"
+    rm $outfilename
+fi
+linecount=0
+while read line; do
+    token=${line%%,*}
+    sound=${line#*,}
+    sound=${sound//[$'\t\r\n']}
+    echo "Token: $token"
+    echo "Sound: $sound"
+    if [[ -z "$sound" ]]; then
+        echo "No sound"
+    else
+        espeak-ng -v mb-en1 -s 150 -g 5 -w "$token.wav" "$sound"
+        ffmpeg -y -i "$token.wav" -c:a mp3 -ac 2 "$token.mp3"
+        rm "$token.wav"
+        echo "$token:robotvoice/$token.mp3[0]" >> $outfilename
+    fi
+done < "$filename"
+echo "Done!"
+# https://en.wikipedia.org/wiki/NATO_phonetic_alphabet
